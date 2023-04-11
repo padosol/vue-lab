04a24b44a4fd74f201cde7c774352f70a297109c
sanghoon <hzyan2@gmail.com>
2023-04-11 11:17:18 +0900

    vue-advanced

---
+++ vue-news/src/views/AskView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="item in ask" v-bind:key="item.id">
+      {{ item.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index';
+
+export default {
+  data() {
+    return {
+      ask:[]
+    }
+  },
+  created() {
+    fetchAskList()
+    .then(response => this.ask = response.data)
+    .catch(function(error){
+      console.log(error)
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-news/src/views/JobsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="job in jobs" v-bind:key="job">
+      {{ job.title  }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchJobsList } from '../api/index';
+
+export default {
+  data(){
+    return{
+      jobs:[]
+    }
+  },
+  created(){
+    fetchJobsList()
+    .then(response => this.jobs = response.data)
+    .catch();
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
