a7fda9877c0100945e159dbb60c737af25b46f10
hsh <hsh@argonet.co.kr>
2023-06-27 23:03:39 +0900

    test

---
+++ vue-lab/src/components/TestView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>test</div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    method: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
