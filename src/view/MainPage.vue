c7c7154910d726d1d8d40afae0a1fa946175ebf5
한상훈 <hzyan2@gmail.com>
2023-09-01 23:02:25 +0900

    test

---
+++ src/view/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+	<div></div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+const { test } = useTest();
+
+function useTest() {
+	console.log(1234);
+	const test = ref();
+
+	return { test };
+}
+</script>
+
+<style lang="scss" scoped></style>
